741d6872fa155c792835695d552541f8f1bfb20f
= <=>
2023-11-26 23:59:01 +0300

    change test chunks

--- sound_controller/test.py
+++ sound_controller/test.py
@@ -6,12 +6,12 @@ CHANNELS = 1  # моно
 RATE = int(audio.get_device_info_by_index(1).get('defaultSampleRate'))
 print(RATE)
 # частота дискретизации - кол-во фреймов в секунду
-CHUNK = 512  # кол-во фреймов за один "запрос" к микрофону - тк читаем по кусочкам
+CHUNK = 128  # кол-во фреймов за один "запрос" к микрофону - тк читаем по кусочкам
 
 
 in_stream = audio.open(format=FORMAT, channels=CHANNELS,
                        rate=RATE, input=True,
-                       frames_per_buffer=CHUNK)
+                       frames_per_buffer=CHUNK*2)
 
 
 
