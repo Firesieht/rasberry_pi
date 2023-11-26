71e199b5d551121ffcd8d995244249b8b748651e
= <=>
2023-11-26 23:13:38 +0300

    change rate

--- sound_controller/test.py
+++ sound_controller/test.py
@@ -1,11 +1,13 @@
 import pyaudio
 
+audio = pyaudio.PyAudio()
 FORMAT = pyaudio.paInt16  # глубина звука = 16 бит = 2 байта
 CHANNELS = 1  # моно
-RATE = 24000  # частота дискретизации - кол-во фреймов в секунду
+RATE = int(audio.get_device_info_by_index(1).get('defaultSampleRate'))
+print(RATE)
+# частота дискретизации - кол-во фреймов в секунду
 CHUNK = 512  # кол-во фреймов за один "запрос" к микрофону - тк читаем по кусочкам
 
-audio = pyaudio.PyAudio()
 
 in_stream = audio.open(format=FORMAT, channels=CHANNELS,
                        rate=RATE, input=True,
