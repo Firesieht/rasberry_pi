c0e1d919df30718f8359a12b4ce69d941dc13d5e
= <=>
2023-12-02 20:54:32 +0300

    change sample rate

--- sound_controller/devices.py
+++ sound_controller/devices.py
@@ -5,5 +5,7 @@ info = p.get_host_api_info_by_index(0)
 numdevices = info.get('deviceCount')
 print(numdevices)
 for i in range(0, numdevices):
+    print('RATE of MIC',int(p.get_device_info_by_index(i).get('defaultSampleRate')))
+
     if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
         print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
