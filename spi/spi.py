18819e6ed45ac20dd428a51e02ef6a80ff5266e8
= <=>
2024-01-16 00:14:02 +0300

    ff

--- spi/spi.py
+++ spi/spi.py
@@ -1,11 +1,22 @@
 import spidev
-
+from datetime import datetime
+from time import sleep
 
 spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.max_speed_hz = 4000000
 NUM_BYTES = 128
 
+now = datetime.now()
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+
+my_file = open(f"{dt_string}.txt", "w+")
+my_file.write("data")
+my_file.close()
+
 while True:
     b  =  spi.readbytes(NUM_BYTES)
-    print(b)
+    my_file = open(f"{dt_string}.txt", "a+")
+    my_file.write(b)
+    my_file.close()
+    sleep(1)
